f39c942dc4c992ceae70c6d0005eccc6788c7b7d
rodri-123 <bigrod_123@yahoo.com>
2019-01-24 23:37:57 -0500

    Modified the for loop from fitty_states to state_list() function

--- Pickling(7).py
+++ Pickling(7).py
@@ -5,19 +5,20 @@ import pickle
 
 api_key = open('api_key.txt').read()
 
-
+# slicing fitty states 
 def state_list():
 	fitty_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
 	return fitty_states[0][1][1:]
 
 
+# Grabing quandl DB and combining with fitty_states
 def grab_initial_state_data():
 	states= state_list()
 	main_df = pd.DataFrame()
 		
-	for abbri in fitty_states[0][1][1:]:
-		query = 'YALE-RBCI'+str(abbri)
-		df = quandl.get("YALE/RBCI",authtoken=api_key)
+	for abbri in state_list():
+		query = 'YALE-RBCI'+abbri
+		df = quandl.get(query,authtoken=api_key)
 		#df.rename(columns={'NSA Value':str(abbri) + ' NSA Value' , 'SA Value' : str(abbri) + ' SA Value'}, inplace=True)
 		
 		if main_df.empty:
@@ -30,7 +31,7 @@ def grab_initial_state_data():
 	pickle.dump(main_df, pickle_out)
 	pickle_out.close()
 
-print(grab_initial_state_data())
+grab_initial_state_data()
 
 '''pickle_in = open('fitty_states', 'rb')
 HPI_data = pickle.load(pickle_in)
